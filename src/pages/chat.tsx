b9203f9673f3cc1fec0c919ca6d410a282d255a7
Maxim Zaytsev <max.zaytsev@team.bumble.com>
2023-01-28 16:38:27 +0000

    Page title added

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,18 +1,25 @@
-import { FC, ReactNode } from 'react';
+import { FC } from 'react';
+import Head from 'next/head'
 import { Grid, Box, Container } from '@mui/material';
 import ChatGpt from '@/components/ChatGpt';
 
 const ChatPage: FC = () => {
     return (
-        <Container maxWidth="md">
-            <Box sx={{ flexGrow: 1 }} height="100vh">
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <ChatGpt/>
+        <>
+            <Head>
+                <title>Chat GPT demo</title>
+                <meta property="og:title" content="Chat GPT demo" key="title" />
+            </Head>
+            <Container maxWidth="md">
+                <Box sx={{ flexGrow: 1 }} height="100vh">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <ChatGpt/>
+                        </Grid>
                     </Grid>
-                </Grid>
-            </Box>
-        </Container>
+                </Box>
+            </Container>
+        </>
     );
 };
 
